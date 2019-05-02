4af5420ce855035eec509fd93255b5951eefd4ba
hansolparkdev <solidev90@gmail.com>
2019-05-02 20:39:27 +0900

    create README.md

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var app = express();
+var http = require("http")
+var server = http.createServer(app);
+var bodyParser = require("body-parser");
+var io = require('socket.io')(server);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+})
+
+
+io.on('connection', function(socket){
+  socket.on('first', function(data){
+    console.log(data.message)
+    socket.emit('second', {
+      message : data.message
+    })
+  })
+})
+
+server.listen(3000, function(){
+  console.log(
+    `
+      socket IO server2 listen on port 3000
+      server start
+    `
+  )
+})
